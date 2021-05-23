dbad84e56b9a98957ac4f5c3009482a19023179d
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-05-23 12:33:21 -0400

    initial setup for frontend and backend

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "us-cdbr-east-03.cleardb.com",
+    USER: "b5c0b7f303ffb9",
+    PASSWORD: "ed672ecc",
+    DB: "heroku_b7cca9d626fcd78"
+};
---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const dbConfig = require("./config.js");
+var connection = mysql.createPool({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+});
+  
+module.exports = connection;
+
+
+
+app.post('/addUser', (req, res) => {
+    const name = req.body.name;
+
+    connection.query('INSERT INTO users (name) VALUES (?)', [name], (err, result)=> {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("success");
+        }
+    })
+});
+
+// simple route
+app.get("/", (req, res) => {
+
+  res.json({ message: "Welcome to bezkoder application." });
+
+});
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
