41aa3a596942e63c04ffbf288232d2db688b9463
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-05-25 13:53:46 -0400

    styling for card and star rating

--- frontend/src/components/TvShow.js
+++ frontend/src/components/TvShow.js
@@ -7,8 +7,12 @@ function TvShow(props) {
     const {result} = props;
 
     useEffect(() => {
-        //console.log(j);
+        //console.log(parseFloat((result.vote_average * 10).toFixed(2)));
     }, []);
+
+    const toPercent = () => {
+        return parseFloat((result.vote_average * 10).toFixed(2)) + "%";
+    }
 
     return (
         <div className="tvShow">
@@ -19,7 +23,7 @@ function TvShow(props) {
             <p className="infoBody">{result.overview}</p>
             <img src={"https://image.tmdb.org/t/p/w200" + result.poster_path} alt={result.name}></img>
             */}
-            <img className="showPic" src={"https://image.tmdb.org/t/p/w400" + result.poster_path} alt={result.name}></img>
+            <img className="showPic" src={"https://image.tmdb.org/t/p/w500" + result.poster_path} alt={result.name}></img>
             
 
             <div className="info">
@@ -27,10 +31,10 @@ function TvShow(props) {
                 <p className="infoBody">{result.first_air_date}</p>
                 <p className="infoBody">{result.vote_average}</p>
                     
-                    <div class="star-ratings-css">
-                    <div class="star-ratings-css-top" style={{width: "100%"}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
-                    <div class="star-ratings-css-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
-                    </div>
+                <div className="starRatings">
+                    <div className="starRatingsTop" style={{width: toPercent()}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+                    <div className="starRatingsBottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+                </div>
             </div>
 
         </div>
