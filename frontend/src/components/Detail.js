3d15d9d9d678fc9630db46a89abe7bb7e1afb73b
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-05-29 22:57:31 -0400

    design and styling for detail

--- frontend/src/components/Detail.js
+++ frontend/src/components/Detail.js
@@ -6,6 +6,12 @@ function Detail(props) {
 
     const [data, setData] = useState({});
 
+    const toPercent = () => {
+        return parseFloat((data.vote_average * 10).toFixed(2)) + "%";
+    }
+
+    
+
     useEffect(() => {
         axios.get("https://api.themoviedb.org/3/tv/" + props.location.id + "?api_key=2c3c49c8f9892c1b17ebf32c4b74bed0&language=en-US")
         .then((res) => {
@@ -19,24 +25,32 @@ function Detail(props) {
             {data !== {} && (
                 <div className="detailPage">
                     <div className="detailBody">
-                        <p>{data.name}</p>
-                        <p>{data.original_name}</p>
-                        <p>year</p>
-                        <p>{data.vote_average}</p>
-                        {data.genres && data.genres.map(x => {
-                            return <p>{x.name}</p>
-                        })}
-                        <p>{data.number_of_seasons}</p>
-                        <p>{data.number_of_episodes}</p>
-                        <p>{data.overview}</p>
-                        {data.seasons && data.seasons.map(x => {
-                            return (
-                                <div>
-                                    <p>{x.name}</p>
-                                    <p>{x.episode_count}</p>
-                                </div>
-                            )
-                        })}
+                        <p className="detailTitle">{data.name}</p>
+                        <p className="bodyText">{data.original_name}</p>
+                        <p className="bodyText">{data.first_air_date}</p>
+
+                        <div className="detailRating">
+                            <p className="vote">{data.vote_average}/10</p>
+                            <div className="starRatings">
+                                <div className="starRatingsTop" style={{width: toPercent()}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+                                <div className="starRatingsBottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+                            </div>
+                        </div>
+
+                        <p>{strGenre()}</p>
+                        <p className="about">!colored right triangle Synopsis</p>
+                        <p className="about">{data.overview}</p>
+
+                        <div className="seasons">
+                            {data.seasons && data.seasons.map(x => {
+                                return (
+                                    <div>
+                                        <p>{x.name}</p>
+                                        <p>{x.episode_count} episodes</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
 
                     <div className="backdrop" style={{backgroundImage: `url(${"https://image.tmdb.org/t/p/original" + data.backdrop_path})`}}></div>
